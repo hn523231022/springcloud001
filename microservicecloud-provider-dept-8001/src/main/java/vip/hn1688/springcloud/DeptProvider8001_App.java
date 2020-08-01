c8963eab8041b7e1a201f28b9dc8e11e19e139b3
szhn523231022 <523231022@qq.com>
2020-08-01 23:41:49 +0800

    springcloud by maybe

---
+++ microservicecloud-provider-dept-8001/src/main/java/vip/hn1688/springcloud/DeptProvider8001_App.java
@@ -0,0 +1,12 @@
+package vip.hn1688.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DeptProvider8001_App
+{
+  public static void main(String[] args)
+  {
+   SpringApplication.run(DeptProvider8001_App.class, args);
+  }
+}
